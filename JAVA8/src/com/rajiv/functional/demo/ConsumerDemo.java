2a7a390c8653b5ee6f8c7eeca57bc411ff1132bd
rajivtalk01 <rajivtalk01@gmail.com>
2022-01-05 15:11:00 +0530

    java 8 lambda and consumer interface demo

---
+++ JAVA8/src/com/rajiv/functional/demo/ConsumerDemo.java
@@ -0,0 +1,25 @@
+package com.rajiv.functional.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * consumer functional demo
+ * @author rajivkumar
+ *
+ */
+public class ConsumerDemo {
+
+	public static void main(String[] args) {
+		/*
+		 * Consumer<Integer> consumer = t -> System.out.println("Printing  : " + t);
+		 * 
+		 * consumer.accept(10);
+		 */
+
+		List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
+
+		list1.stream().forEach(t -> System.out.println("print  : " + t));
+
+	}
+}
